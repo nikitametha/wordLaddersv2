bbb68efef37141fd7eb4ef20641ddc17a3b82980
Nikita Metha <nikitametha@gmail.com>
2023-08-02 17:26:22 -0700

    Added Play Game feature and associated html,css,js and python logic
    Enabled 'Play The Game' button to show the start ande end word, and button to begin gameplay. Game is played over at playgame.html which allows user to enter next word guess, gives hint of how to get the next word(add,change,delete) and gives user the option to retrieve next word . Also, list of currentt ladder is displayed. On winning, game redirects to homepage.

--- src/game_flask.py
+++ src/game_flask.py
@@ -3,6 +3,13 @@ import random
 import networkx as nx
 from flask import Flask,request, render_template
 
+
+x=[0,0]
+g=[]
+hintt=0
+won=0
+helpguess=""
+
 reader= open('../data/word_data.txt', 'r')
 with open("../data/preprocessed_words.txt", "rb") as fp:   # Unpickling
    b = pickle.load(fp)
@@ -20,11 +27,10 @@ for words in reader:
 for k,v in b.items():
     for v_i in v:
         net.add_edge(k,v_i)
-x=[]
-ll=[]
-ll2=[]
+
 
 app = Flask(__name__,template_folder='templates', static_folder='static')
+
 
 @app.route("/")
 def index():
@@ -32,19 +38,23 @@ def index():
         try:
             f1=random.choice(word_list)
             f2=random.choice(word_list)
+            global ll
             ll=nx.shortest_path(net, f1, f2)
             print("Start Word: ",f1)
             print("End Word: ",f2)
         except nx.NetworkXNoPath:
             continue
         break
-    x=[]
-    x.append(f1)
-    x.append(f2)
+    x[0]=f1
+    x[1]=f2
+    global leng
     leng=len(ll)
-    ll2=" ".join(ll)
-    
-    return render_template("index.html",x=x,leng=leng,ll=ll2)
+    print(ll)
+    global rr, won, g
+    rr=1
+    won=0
+    g=[]
+    return render_template("index.html",x=x,leng=leng,ll=ll)
 
 
 @app.route('/result',methods = ['POST', 'GET'])
@@ -55,11 +65,66 @@ def result():
       w2=(result['endwrd'])
       if(w1 in word_list and w2 in word_list):
           try: res=nx.shortest_path(net, w1, w2)
-          except nx.NetworkXNoPath: res2=-1
-      else: res2=0
+          except nx.NetworkXNoPath: res2=-1 # there's no path between the words
+      else: res2=0 #either or both words not in our wordlist data
       res1=(res)
-      
+
       return render_template("result.html",res1 = res1, res2=res2)
+
+@app.route('/playgame',methods = ['POST', 'GET'])
+def playgame():
+    print("in playgame the words are = ",x[0])
+    print("the ladder is  = ", ll)
+    return render_template("playgame.html",x=x,ll=ll)
+
+
+@app.route('/calculate',methods = ['POST', 'GET'])
+def calculate():
+    global won
+    global rr
+    if request.method == 'POST':
+        #hint next word button is pressed
+        if request.form['submitbutton'] == 'Show me the next word':
+            hintt =ll[rr]
+            g.append(ll[rr])
+            rr+=1
+            if(rr==len(ll)):  
+                print("won in hints!")
+                won=1
+                return render_template("index.html",x=x,ll=ll)
+            else:
+                print("didn't win just yet")
+                return render_template("playgame.html",x=x,ll=ll,hintt=hintt,won=won,g=g)
+        if request.form['submitbutton'] == 'Check Word!':
+            print("entered to check word")
+            result = request.form
+            t=result['guesswrd']
+            global helpguess
+            if (t in ll and t not in g):
+                print("new guess")
+                g.append(t)
+                rr=rr+1
+                print(g)
+                if(rr==len(ll)):
+                    print("won!")
+                    won=1
+                    return render_template("index.html",x=x,ll=ll)
+                else:
+                    print("correct guess")
+                    helpguess="Correct Guess!"
+            else:
+                print("incorrect word, give hint")
+                if(len(ll[rr-1])<len(ll[rr])):
+                    helpguess="Try again! Add a letter!"
+                elif(len(ll[rr-1])>len(ll[rr])):
+                    helpguess="Try again! Delete a letter"
+                else:
+                    helpguess="Try again! Change a letter!"
+            print("rendering template")
+            return render_template("playgame.html",x=x,ll=ll,won=won,helpguess=helpguess,g=g)
+
+
+
 
 if __name__ == '__main__':
    app.run(port=8000,debug = True)
--- src/static/index.js
+++ src/static/index.js
@@ -1,6 +1,6 @@
-// Get the modal
+// Functionality for 'How to Play' button
 
-var modal = document.getElementById("myModal");
+var helpmodal = document.getElementById("helpModal");
 // Get the button that opens the modal
 var abtbtn = document.getElementById("about");
 
@@ -9,23 +9,23 @@ var abtspan = document.getElementById("close-modal");
 
 // When the user clicks on the button, open the modal
 abtbtn.onclick = function() {
-  modal.style.display = "block";
+  helpmodal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 abtspan.onclick = function() {
-  modal.style.display = "none";
+  helpmodal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    helpmodal.style.display = "none";
   }
 }
 
 
-
+// Functionality for 'word ladder solver' button
 var solvermodal = document.getElementById("solverForm");
 var solverbtn = document.getElementById("solver");
 
@@ -48,6 +48,35 @@ window.onclick = function(event) {
     solvermodal.style.display = "none";
   }
 }
+
+
+// Functionality for 'play the game' button
+
+var playmodal = document.getElementById("playModal");
+// Get the button that opens the modal
+var playbtn = document.getElementById("playgame");
+
+// Get the <span> element that closes the modal
+var playspan = document.getElementById("close-playmodal");
+
+// When the user clicks on the button, open the modal
+playbtn.onclick = function() {
+  playmodal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+playspan.onclick = function() {
+  playmodal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    playmodal.style.display = "none";
+  }
+}
+
+
 
 /*
 function hardlvl()
